d2f288741aca2a440ee69784c47154197222e922
marcos ventura <marcosventura.dev@gmail.com>
2023-09-09 09:35:33 -0300

    how to draw a image

--- script.js
+++ script.js
@@ -10,12 +10,12 @@ const playerImage = new Image();
 
 playerImage.src = 'shadow_dog.png';
 
-let x = 0;
+
 
 function animate(){
     ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(x,50,100,100);
-    x++;
+   // ctx.fillRect(50,50,100,100);
+    ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     requestAnimationFrame(animate);
 };
 
